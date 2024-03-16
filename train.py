9e447141c87fae72cb07e49b5b7a35971bea269f
Char <char543@ymail.com>
2024-03-16 00:52:51 -0400

    test

--- train.py
+++ train.py
@@ -56,7 +56,21 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        out = t.cat([h(x) for h in self.heads], dim=-1) #concatenates all of the outputes
+        out = t.cat([h(x) for h in self.heads], dim=-1) #concatenates all of the outputs
         out = self.dropout(self.proj(out))
         return out
+        
+class FeedFoward(nn.Module):
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
     
