4a9477ac9f9e875a1b3a835c8610905135a7ed57
emilyrhee <emilyrhee99@gmail.com>
2024-03-28 16:00:51 -0400

    reordered code
    Head comes before language model

--- v2.py
+++ v2.py
@@ -58,6 +58,73 @@ def estimate_loss():
     m.train()
     return out
 
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', t.tril(t.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout) # dropout randomly drops, or zeroes, some elements during training
+        
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x) # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T), softmax is normalization operation.Rescales input tensors, so output tensors lie in range [0,1] and sum to 1
+        wei = self.dropout(wei)
+        
+        v = self.value(x) # (B,T,hs)
+        out = wei @v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
+      
+class MultiHeadAttention(nn.Module):
+    #multiple self attention heads running in parallel
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = t.cat([h(x) for h in self.heads], dim=-1) #concatenates all of the outputs. 
+        out = self.dropout(self.proj(out)) #applies projection
+        return out
+        
+class FeedFoward(nn.Module):
+    #single layer linear, followed by a relu non-linearity
+    #if self attention is communication, this is thinking upon it individually
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class Block(nn.Module):
+    #intersperses communication and computation 
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size) #communication
+        self.ffwd = FeedFoward(n_embd) #computation
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x)) #communication
+        x = x + self.ffwd(self.ln2(x)) #computation
+        return x
+
 #Simplest possible neural network, the Bigram Language Model
 class BigramLanguageModel(nn.Module):
     def __init__(self):
@@ -117,70 +184,3 @@ for steps in range(max_iters):
 # generate from the BigramLanguageModel
 context = t.zeros((1,1), dtype = t.long)  # start generating from a single 0
 print(decode(m.generate(context, max_new_tokens)[0].tolist()))
-
-class Head(nn.Module):
-    def __init__(self, head_size):
-        super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', t.tril(t.ones(block_size, block_size)))
-        self.dropout = t.Dropout(dropout) # dropout randomly drops, or zeroes, some elements during training
-        
-    def forward(self, x):
-        B,T,C = x.shape
-        k = self.key(x) # (B,T,hs)
-        q = self.query(x) # (B,T,hs)
-        
-        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
-        wei = F.softmax(wei, dim=-1) # (B, T, T), softmax is normalization operation.Rescales input tensors, so output tensors lie in range [0,1] and sum to 1
-        wei = self.dropout(wei)
-        
-        v = self.value(x) # (B,T,hs)
-        out = wei @v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
-        return out
-      
-class MultiHeadAttention(nn.Module):
-    #multiple self attention heads running in parallel
-    def __init__(self, num_heads, head_size):
-        super().__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        self.proj = nn.Linear(head_size * num_heads, n_embd)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        out = t.cat([h(x) for h in self.heads], dim=-1) #concatenates all of the outputs. 
-        out = self.dropout(self.proj(out)) #applies projection
-        return out
-        
-class FeedFoward(nn.Module):
-    #single layer linear, followed by a relu non-linearity
-    #if self attention is communication, this is thinking upon it individually
-    def __init__(self, n_embd):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(n_embd, 4 * n_embd),
-            nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd),
-            nn.Dropout(dropout),
-        )
-
-    def forward(self, x):
-        return self.net(x)
-    
-class Block(nn.Module):
-    #intersperses communication and computation 
-    def __init__(self, n_embd, n_head):
-        # n_embd: embedding dimension, n_head: the number of heads we'd like
-        super().__init__()
-        head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size) #communication
-        self.ffwd = FeedFoward(n_embd) #computation
-        self.ln1 = nn.LayerNorm(n_embd)
-        self.ln2 = nn.LayerNorm(n_embd)
-
-    def forward(self, x):
-        x = x + self.sa(self.ln1(x)) #communication
-        x = x + self.ffwd(self.ln2(x)) #computation
-        return x
